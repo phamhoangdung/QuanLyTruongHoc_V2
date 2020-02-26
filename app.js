064a4371b89cf198ef9377f7a812c6bfb4b338e3
phamhoangdung <33390290+phamhoangdung@users.noreply.github.com>
2020-02-26 14:19:45 +0700

    fix

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var acl = require('./config/ACL.Config');
 //===================!admin=================
 
 //===================api=================
-var indexapi = require('./routes/api/index');
+var indexapi = require('./routes/api/Index.Route');
 var QLHocSinhapi = require('./routes/api/QLHocSinh.Route');
 var QLLopHocapi = require('./routes/api/QLLopHoc.Route');
 var QLTaiKhoanapi = require('./routes/api/QLTaiKhoan.Route');
